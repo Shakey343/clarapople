6f88fea5b8459b9444afbeb17fefdd4c90a54c74
Jake Pople <jakepople@hotmail.co.uk>
2024-02-06 17:08:59 +0000

    subscription form created and working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
+
 const Footer = () => {
+  const [email, setEmail] = useState();
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setEmail(value);
+  };
+
+  const handleSubscribe = (event) => {
+    event.preventDefault();
+    console.log(
+      `backend: ${import.meta.env.VITE_BACKEND_URL}. Email: ${email}`
+    );
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/users/subscribe`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({email}),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((e) => {
+        console.log("error", e);
+      });
+  };
+
   return (
     <div className="absolute w-full bottom-0 text-center sm:text-left flex flex-col items-center justify-between px-10 py-5 text-white font-mono md:items-end md:flex-row">
       <div className="lg:ml-36">
         <h2 className="text-2xl">Subscribe to my Newsletter</h2>
-        <form action="" className="border-2 border-white bg-black/50 flex justify-between px-4 focus">
-          <input type="email" placeholder="Email Address" className="py-2 bg-transparent focus:outline-none" />
+        <form
+          onSubmit={handleSubscribe}
+          className="border-2 border-white bg-black/50 flex justify-between px-4 focus"
+        >
+          <input
+            type="email"
+            onChange={handleChange}
+            placeholder="Email Address"
+            className="py-2 bg-transparent focus:outline-none"
+          />
           <button type="submit">Sign Up</button>
         </form>
       </div>
