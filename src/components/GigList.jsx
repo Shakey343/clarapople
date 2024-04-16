22fd639ab7bb5b6f5b0eea5d4e42245c8ea8e925
Jake Pople <jakepople@hotmail.co.uk>
2024-04-16 16:11:36 +0100

    gigs calendar functionality working

--- src/components/GigList.jsx
+++ src/components/GigList.jsx
@@ -1,18 +1,59 @@
+import { useState } from "react";
 import GigListItem from "./GigListItem";
-import gigList from "./gigs.json"
+import gigList from "./gigs.json";
 
 const GigList = () => {
-  console.log(gigList)
+  const today = new Date();
+  const [monthNo, setMonthNo] = useState(today.getMonth());
 
-  const mappedGigList = gigList.map((gig, i) => {
-      return (
-        <GigListItem key={i} gig={gig} />
-      );
+  const date = new Date();
+  date.setMonth(monthNo);
+  const monthString = date.toLocaleString("default", {
+    month: "long",
+    year: "2-digit",
   });
+  const monthStr = date.toLocaleString("default", {
+    month: "short",
+    year: "2-digit",
+  });
+
+  const handleAddMonth = () => {
+    setMonthNo((prevNo) => prevNo + 1);
+  };
+  const handleMinusMonth = () => {
+    setMonthNo((prevNo) => prevNo - 1);
+  };
+
+  const groupedGigs = Object.groupBy(gigList, ({ date }) => {
+    const d = new Date(date);
+    return d.getMonth();
+  });
+
+  const noGigMessage = (
+    <tr
+      className={
+        "border-y border-white/40 bg-black/30 hover:bg-black/60 cursor-pointer ease-in-out duration-100"
+      }
+      onClick={() => setMonthNo(today.getMonth())}
+    >
+      <td className="p-4 text-center" colSpan="3">
+        No gigs found 🤷
+        <br />
+        {"<-"} Return to{" "}
+        {today.toLocaleString("default", { month: "long", year: "2-digit" })}
+      </td>
+    </tr>
+  );
+
+  const mappedGigList = groupedGigs[monthNo]
+    ? groupedGigs[monthNo].map((gig, i) => {
+        return <GigListItem key={i} gig={gig} />;
+      })
+    : noGigMessage;
 
   return (
     <div className="w-full sm:w-[600px] text-slate-50 drop-shadow-lg">
-      <h2 className="text-4xl">Upcoming Gigs</h2>
+      <h2 className="text-4xl">Gigs - {monthString}</h2>
       <table className="w-full text-left mt-5 table-auto">
         <thead className="text-sm text-white/70">
           <tr className="w-full">
@@ -23,6 +64,11 @@ const GigList = () => {
         </thead>
         <tbody>{mappedGigList}</tbody>
       </table>
+      <div className="flex justify-center w-100">
+        <button onClick={handleMinusMonth}>{"<-"}&nbsp;</button>
+        <span>{monthStr}</span>
+        <button onClick={handleAddMonth}>&nbsp;{"->"}</button>
+      </div>
     </div>
   );
 };
--- src/components/GigListItem.jsx
+++ src/components/GigListItem.jsx
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react";
+import cn from "../../utils/cn";
 
-const GigListItem = ({gig}) => {
-  const [old, setOld] = useState(false)
+const GigListItem = ({ gig }) => {
+  const [old, setOld] = useState(false);
 
   let today = new Date();
-  let date = new Date(gig.date)
+  let date = new Date(gig.date);
   let time = date.toLocaleString("en-US", { hour: "numeric", hour12: true });
 
   useEffect(() => {
     if (date < today) {
-      // Strikethrough old dates
-      setOld(true)
-      console.log("old date");
+      setOld(true);
+      // 1. Connect to google drive api to create array of gigs
+      // 2. Sort the full list of gigs into chronilogical order
+      // 3. Only show gigs from today onwards (month by month)
+      // 4. Create way to cycle through pages of gigs (past & future)
+      // links at bottom should say: <- Jan 24 ->
     }
-  }, [])
-
+  }, []);
 
   return (
     <tr
-      className={`border-y border-white/40 bg-black/30 hover:bg-black/60 cursor-pointer ease-in-out duration-100 ${old && 'line-through'}`}
+      className={cn(
+        "border-y border-white/40 bg-black/30 hover:bg-black/60 cursor-pointer ease-in-out duration-100",
+        old && "line-through"
+      )}
       onClick={() => window.open(gig.link, "_blank")}
     >
       <td className="p-4">
