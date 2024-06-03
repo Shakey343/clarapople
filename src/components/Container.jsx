af078a4dff98a8888bfa74272b9817355060516f
Jake Pople <jakepople@hotmail.co.uk>
2024-06-03 16:12:34 +0100

    updated styling

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,11 @@
-import cn from '../../utils/cn';
+import cn from "../../utils/cn";
 
 const Container = ({ className, children }) => {
-  return <div className={cn('mx-auto max-w-screen mx-6 lg:px-10', className)}>{children}</div>;
+  return (
+    <div className={cn("mx-auto max-w-screen mx-6 lg:px-10", className)}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
